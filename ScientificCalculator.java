6b948631c47253d7b691d2d123b5bacef5438064
Eklas <alsiyabieklas9@gmail.com>
2025-07-21 09:24:16 +0400

    Java Math Class

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -19,6 +19,110 @@ public class ScientificCalculator {
         System.out.println("Your age" + age + "years old.");
         System.out.println("And you are " + H + " meters tall.");
 
+        boolean running = true;
+        while (running) {
+            displayMenu();
+            System.out.println("Select a Calculation :");
+            int choice = -1;
+            try {
+                choice = IpReader.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter the number only!");
+                IpReader.next();
+                continue;
+            }
+            switch (choice) {
+                case 1:
+                    performAddition(IpReader);
+                    break;
+                case 2:
+                    performSubtraction(IpReader);
+                    break;
+                case 3:
+                    performMultiplication(IpReader);
+                    break;
+                case 4:
+                    performDivision(IpReader);
+                    break;
+                case 5:
+                    performSquareRoot(IpReader);
+                    break;
+                case 6:
+                    performPower(IpReader);
+                    break;
+                case 7:
+                    performSine(IpReader);
+                    break;
+                case 8:
+                    performCosine(IpReader);
+                    break;
+                case 9:
+                    performTangent(IpReader);
+                    break;
+                case 0:
+                    running = false;
+                    System.out.println("Exited from calculator Thank you");
+                    break;
+                default:
+                    System.out.println("Oops, wrong choice.!");
+
+            }
+        }
+
+        IpReader.close();
+    }
+
+    public static void displayMenu() {
+        System.out.println("Operations list");
+        System.out.println("1. +");
+        System.out.println("2. -");
+        System.out.println("3. * ");
+        System.out.println("4. /");
+        System.out.println("5. square root  ");
+        System.out.println("6. ^");
+        System.out.println("7. sin");
+        System.out.println("8. cos");
+        System.out.println("9. tan");
+        System.out.println("0. exit");
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+
+    public static double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public static double sine(double angleDegrees) {
+        return Math.sin(Math.toRadians(angleDegrees));
+    }
+
+    public static double cosine(double angleDegrees) {
+        return Math.cos(Math.toRadians(angleDegrees));
+    }
+
+    public static double tangent(double angleDegrees) {
+        return Math.tan(Math.toRadians(angleDegrees));
+    }
+
+
 
         IpReader.close();
     }
